ad8777753dbb61100b06649c42083b4ab9d9276b
roman <roman.khomyk@gmail.com>
2020-07-30 10:58:27 +0300

    fix decimal precision in statistics; fix market data;

---
+++ src/AlgoserverAPI/Migrations/20200730075018_FixDecimalPrecision.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Algoserver.API.Migrations
+{
+    public partial class FixDecimalPrecision : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StopLossRatio",
+                table: "Statistics",
+                type: "decimal(18,3)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SplitPositions",
+                table: "Statistics",
+                type: "decimal(18,3)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RiskOverride",
+                table: "Statistics",
+                type: "decimal(18,3)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AccountSize",
+                table: "Statistics",
+                type: "decimal(18,3)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StopLossRatio",
+                table: "Statistics",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SplitPositions",
+                table: "Statistics",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RiskOverride",
+                table: "Statistics",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AccountSize",
+                table: "Statistics",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)");
+        }
+    }
+}
