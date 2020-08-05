31abc314c10b58a88ef9193c53355396055bd352
Andriy Yarosh <andriy.yarosh@gmail.com>
2020-08-05 13:14:17 +0300

    history loading improving

--- src/AlgoserverAPI/Services/HistoryService.cs
+++ src/AlgoserverAPI/Services/HistoryService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -17,7 +18,7 @@ namespace Algoserver.API.Services
     // Provide historical data from oanda or twelvedata datafeeds
     public class HistoryService
     {
-        private const int BARS_COUNT = 1000;
+        private const int BARS_COUNT = 600;
         private const int MIN_BARS_COUNT = 400;
         private const int ONE_DAY_TIME_SHIFT = 60 * 60 * 24;
 
@@ -38,14 +39,15 @@ namespace Algoserver.API.Services
         }
 
         public async Task<HistoryData> GetHistory(string symbol, int granularity, string datafeed, string exchange, string type, int replayBack = 0)
-        {   
+        {
             var hash = getHash(symbol, granularity, datafeed, exchange);
 
             try
             {
                 if (_cache.TryGetValue(hash, out HistoryData cachedResponse))
                 {
-                    if (cachedResponse.Bars != null && cachedResponse.Bars.Count() - replayBack >= MIN_BARS_COUNT) {
+                    if (cachedResponse.Bars != null && cachedResponse.Bars.Count() - replayBack >= MIN_BARS_COUNT)
+                    {
                         return cachedResponse;
                     }
                 }
@@ -58,21 +60,128 @@ namespace Algoserver.API.Services
 
             var barsBack = BARS_COUNT + replayBack;
 
-            // less than 1 day (8 working hours + 2 weekend)
-            if (granularity < 86400) {
-                if (type == "forex" || type == "crypto" || exchange == "oanda") {
-                    barsBack *= 2;
-                } else {
-                    barsBack *= 5;
+            var result = await LoadHistoricalData(datafeed, symbol, granularity, barsBack, exchange);
+
+            try
+            {
+                if (result != null && result != null && result.Bars != null && result.Bars.Any())
+                {
+                    if (granularity > 60)
+                    {
+                        _cache.Set(hash, result, TimeSpan.FromMinutes(3));
+                    }
+                    else
+                    {
+                        _cache.Set(hash, result, TimeSpan.FromMinutes(1));
+                    }
                 }
             }
-            
-            long endDate = AlgoHelper.UnixTimeNow() + (60 * 60 * 12); // + 12h to prevent TZ difference
-            long startDate = endDate - (barsBack * granularity);
+            catch (Exception e)
+            {
+                _logger.LogError("Failed to set cached response");
+                _logger.LogError(e.Message);
+            }
+
+            return result;
+        }
+
+        private async Task<HistoryData> LoadHistoricalData(string datafeed, string symbol, int granularity, long bars_count, string exchange = "")
+        {
+            var exists = _contextAccessor.HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader);
+            var bearerString = authHeader.ToString();
+            var bearerdatafeed = datafeed.ToLowerInvariant();
+
+            var result = new HistoryData {
+                Bars = new List<BarMessage>(),
+                Datafeed = datafeed,
+                Exchange = exchange,
+                Granularity = granularity,
+                Symbol = symbol
+            };
+
+            var requestCount = 0;
+
+            do
+            {
+                var endDate = this.getEndDate(result);
+                var startDate = this.getStartDate(result, bars_count);
+
+                var uri = $"{_serverUrl}/{bearerdatafeed}/history?" +
+                          $"kind=daterange&symbol={symbol}&granularity={granularity}&from={startDate}&to={endDate}";
+
+                if (string.IsNullOrWhiteSpace(exchange))
+                {
+                    uri = $"{uri}&exchange={exchange}";
+                }
+
+                var request = new HttpRequestMessage(HttpMethod.Get, uri)
+                {
+                    Headers = {
+                    { HttpRequestHeader.Authorization.ToString(), bearerString }
+                }
+                };
+
+                var response = await _httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogError($"Error occured while sending http request to {request.RequestUri}, response string: {content}");
+                    response.EnsureSuccessStatusCode();
+                }
+
+                IEnumerable<BarMessage> bars = null;
+                if (bearerdatafeed == "oanda")
+                {
+                    var oandaResponse = JsonConvert.DeserializeObject<OandaHistoryResponse>(content);
+                    bars = oandaResponse.Data.Bars;
+                }
+                else
+                {
+                    var historyResponse = JsonConvert.DeserializeObject<HistoryData>(content);
+                    bars = historyResponse.Bars;
+                }
+                result.Bars = this.mergeBars(result.Bars, bars);
+
+                if (requestCount++ > 5) {
+                    return result;
+                }
+
+            } while (result.Bars.Count() < bars_count);
+
+            return result;
+        }
+
+        private IEnumerable<BarMessage> mergeBars(IEnumerable<BarMessage> existingBars, IEnumerable<BarMessage> newBars) {
+            var firstBar = existingBars.FirstOrDefault();
+
+            if (firstBar == null) {
+                return newBars;
+            }
+
+            var barsToAppend = newBars.Where(_ => _.Timestamp < firstBar.Timestamp).ToList();
+            barsToAppend.AddRange(existingBars);
+            return barsToAppend;
+        }
+
+        private long getEndDate(HistoryData data) {
+            var firstBar = data.Bars.FirstOrDefault();
+
+            if (firstBar == null) {
+                return AlgoHelper.UnixTimeNow() + (60 * 60 * 12); // + 12h to prevent TZ difference
+            }
+
+            return firstBar.Timestamp + 1;
+        } 
+        
+        private long getStartDate(HistoryData data, long bars_count) {
+            var existing_count = data.Bars.Count();
+            var endDate = this.getEndDate(data);
+            long startDate = endDate - ((bars_count - existing_count) * data.Granularity * 3);
             int day = new DateTime(startDate).Day;
 
             // load more minute data
-            if (granularity < 3600)
+            if (data.Granularity < 3600)
             {
                 startDate = startDate - ONE_DAY_TIME_SHIFT;
             }
@@ -88,69 +197,13 @@ namespace Algoserver.API.Services
                 startDate = startDate - ONE_DAY_TIME_SHIFT;
             }
 
-            var result = await LoadHistoricalData(datafeed, symbol, granularity, startDate, endDate, exchange);
-
-            try
+            var count = (endDate - startDate) / data.Granularity;
+            if (count > 5000)
             {
-                if (result != null && result != null && result.Bars != null && result.Bars.Any())
-                {
-                    if (granularity > 60) {
-                        _cache.Set(hash, result, TimeSpan.FromMinutes(3));
-                    } else {
-                        _cache.Set(hash, result, TimeSpan.FromMinutes(1));
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                _logger.LogError("Failed to set cached response");
-                _logger.LogError(e.Message);
+                startDate = endDate - (data.Granularity * 5000);
             }
 
-            return result;
-        }
-
-        private async Task<HistoryData> LoadHistoricalData(string datafeed, string symbol, int granularity, long startDate, long endDate, string exchange = "")
-        {
-            var exists = _contextAccessor.HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader);
-            var bearerString = authHeader.ToString();
-            var bearerdatafeed = datafeed.ToLowerInvariant();
-
-            if (!exists || string.IsNullOrEmpty(bearerString)) {
-
-            }
-
-            var uri = $"{_serverUrl}/{bearerdatafeed}/history?" +
-                      $"kind=daterange&symbol={symbol}&granularity={granularity}&from={startDate}&to={endDate}";
-
-            if (string.IsNullOrWhiteSpace(exchange))
-            {
-                uri = $"{uri}&exchange={exchange}";
-            }
-
-            var request = new HttpRequestMessage(HttpMethod.Get, uri) {
-                 Headers = {
-                    { HttpRequestHeader.Authorization.ToString(), bearerString }
-                }
-            };
-
-            var response = await _httpClient.SendAsync(request);
-            var content = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                _logger.LogError($"Error occured while sending http request to {request.RequestUri}, response string: {content}");
-                response.EnsureSuccessStatusCode();
-            }
-
-            if (bearerdatafeed == "oanda") {
-                var oandaResponse = JsonConvert.DeserializeObject<OandaHistoryResponse>(content);
-                return oandaResponse.Data;
-            } else {
-                var historyResponse = JsonConvert.DeserializeObject<HistoryData>(content);
-                return historyResponse;
-            }
-
+            return startDate;
         }
 
         private string getHash(string symbol, int granularity, string datafeed, string exchange = "")
