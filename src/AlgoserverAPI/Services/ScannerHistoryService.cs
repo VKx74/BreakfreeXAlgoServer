8377cb43f66af6933702226b305da204973971cc
andriy.yarosh <andriy.yarosh@gmail.com>
2020-10-09 18:48:51 +0300

    loading by packages

--- src/AlgoserverAPI/Services/ScannerHistoryService.cs
+++ src/AlgoserverAPI/Services/ScannerHistoryService.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Algoserver.API.Models.REST;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Algoserver.API.Services
 {
+    class HistoryRequest {
+        public string Symbol { get; set; }
+        public string Datafeed { get; set; }
+        public string Exchange { get; set; }
+        public int Granularity { get; set; }
+        public int Count { get; set; }
+    }
+
     public class ScannerHistoryService
     {
         private readonly HistoryService _historyService;
@@ -23,36 +32,52 @@ namespace Algoserver.API.Services
         {
             var forexInstruments = _instrumentService.GetOandaInstruments();
             var stopWatch = new Stopwatch();
-            var tasks15min = new List<Task<HistoryData>>();
-            var tasks1h = new List<Task<HistoryData>>();
-            var tasks4h = new List<Task<HistoryData>>();
+            var tasks15min = new List<HistoryRequest>();
+            var tasks1h = new List<HistoryRequest>();
+            var tasks4h = new List<HistoryRequest>();
 
             foreach (var instrument in forexInstruments)
             {
                 var Exchange = instrument.Datafeed.ToLowerInvariant();
                 var Datafeed = instrument.Datafeed.ToLowerInvariant();
                 var Symbol = instrument.Symbol;
-                
-                var hour4 = _historyService.GetHistory(Symbol, TimeframeHelper.HOUR4_GRANULARITY, Datafeed, Exchange, 300);
-                var hourly = _historyService.GetHistory(Symbol, TimeframeHelper.HOURLY_GRANULARITY, Datafeed, Exchange, 300);
-                var min15 = _historyService.GetHistory(Symbol, TimeframeHelper.MIN15_GRANULARITY, Datafeed, Exchange, 400);
-                tasks15min.Add(min15);
-                tasks1h.Add(hourly);
-                tasks4h.Add(hour4);
+                tasks15min.Add(new HistoryRequest {
+                    Symbol = Symbol,
+                    Datafeed = Datafeed,
+                    Exchange = Exchange,
+                    Count = 300,
+                    Granularity = TimeframeHelper.MIN15_GRANULARITY
+                });
+                tasks1h.Add(new HistoryRequest {
+                    Symbol = Symbol,
+                    Datafeed = Datafeed,
+                    Exchange = Exchange,
+                    Count = 300,
+                    Granularity = TimeframeHelper.HOURLY_GRANULARITY
+                });
+                tasks4h.Add(new HistoryRequest {
+                    Symbol = Symbol,
+                    Datafeed = Datafeed,
+                    Exchange = Exchange,
+                    Count = 300,
+                    Granularity = TimeframeHelper.HOUR4_GRANULARITY
+                });
             }
             
             stopWatch.Start();
-            var min15task = await Task.WhenAll<HistoryData>(tasks15min);
+            var min15task = await this._loadPack(tasks15min);
             stopWatch.Stop();
             TimeSpan ts15 = stopWatch.Elapsed;
 
+            stopWatch.Reset();
             stopWatch.Start();
-            var hourlytask = await Task.WhenAll<HistoryData>(tasks1h);
+            var hourlytask = await this._loadPack(tasks1h);
             stopWatch.Stop();
             TimeSpan ts1h = stopWatch.Elapsed;
 
+            stopWatch.Reset();
             stopWatch.Start();
-            var hour4task = await Task.WhenAll<HistoryData>(tasks4h);
+            var hour4task = await this._loadPack(tasks4h);
             stopWatch.Stop();
             TimeSpan ts4h = stopWatch.Elapsed;
 
@@ -64,6 +89,26 @@ namespace Algoserver.API.Services
 
             return elapsedTime15 + " - " + elapsedTime1h  + " - " + elapsedTime4h;
             
+        }
+
+        private async Task<List<HistoryData>> _loadPack(List<HistoryRequest> tasks) {
+            var result = new List<HistoryData>();
+            var count = 20;
+
+            while(tasks.Count > 0) {
+                var tasksToProcess = tasks.Take(count);
+                tasks.RemoveRange(0, Math.Min(count, tasks.Count));
+                
+                var tasksToWait = new List<Task<HistoryData>>();
+                foreach (var t in tasksToProcess) {
+                    var task = _historyService.GetHistory(t.Symbol, t.Granularity, t.Datafeed, t.Exchange, t.Count);
+                    tasksToWait.Add(task);
+                }
+                var res = await Task.WhenAll<HistoryData>(tasksToWait);
+                result.AddRange(res);
+            }
+
+            return result;
         }
     }
 }
