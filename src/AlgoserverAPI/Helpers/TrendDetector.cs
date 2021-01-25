7b2187da0e9dcdf7e3d76a45be2a0ecdbaeb4365
andriy.yarosh <andriy.yarosh@gmail.com>
2021-01-25 19:18:42 +0200

    RTD trend straight calculation changed

--- src/AlgoserverAPI/Helpers/TrendDetector.cs
+++ src/AlgoserverAPI/Helpers/TrendDetector.cs
@@ -47,8 +47,8 @@ namespace Algoserver.API.Helpers
         public static TrendsStrengthResult MeasureTrendsStrength(MESAData[] mesa_global, MESAData[] mesa_local) {
             var length = mesa_global.Length;
             var count = mesa_global.Length / 2;
-            if (count > 200) {
-                count = 200;
+            if (count > 100) {
+                count = 100;
             }
 
             var global_spreads = new List<decimal>();
