ccd0d1ff3a9284493c7295fbd0226388e3068df6
andriy.yarosh <andriy.yarosh@gmail.com>
2023-06-20 20:39:56 +0300

    static token for fxstreet

--- src/AlgoserverAPI/Services/EconomicCalendarService.cs
+++ src/AlgoserverAPI/Services/EconomicCalendarService.cs
@@ -42,20 +42,21 @@ namespace Algoserver.API.Services
         public async Task LoadEconomicEvents()
         {
             // load token
-            HttpClient tokenClient = new HttpClient();
-            var tokenResponse = await tokenClient.GetAsync("https://prod-stockserver.smarttrader.com:8091/GetAuthToken?key=ce99d5346dcbaade596123ac91d46b2c");
-            if (!tokenResponse.IsSuccessStatusCode)
-            {
-                Console.WriteLine(tokenResponse.StatusCode);
-                Console.WriteLine(">>> Error: Failed to load economical calendar token");
-                return;
-            }
-            var token = await tokenResponse.Content.ReadAsStringAsync();
-            token = token.Trim('"');
+            // HttpClient tokenClient = new HttpClient();
+            // var tokenResponse = await tokenClient.GetAsync("https://prod-stockserver.smarttrader.com:8091/GetAuthToken?key=ce99d5346dcbaade596123ac91d46b2c");
+            // if (!tokenResponse.IsSuccessStatusCode)
+            // {
+            //     Console.WriteLine(tokenResponse.StatusCode);
+            //     Console.WriteLine(">>> Error: Failed to load economical calendar token");
+            //     return;
+            // }
+            // var token = await tokenResponse.Content.ReadAsStringAsync();
+            // token = token.Trim('"');
+            var token = "bearer pH7gaT7d2sRCd7EDGt4vJfnpxL0CqfIBmh-3J6SZo3hCv7qCecfCE_z85Lud9WKSzVWwuYUlTgLYR0YGoCkwyEiaPlZyPYrj2g13CDo66c_AxLOqKgg2-D8QubL63B49M5LiqDUDALnk3EhtXsfYszqFrLeqnTb-wKJuTgSTkhYv3YU2DYFzbUPxImhDv_FBd1ZgIN0UUmA2EaVFxiB3tMaIWjQTk9DOP-qruQaxgIPp62VOl7E64DtUVTyMnSasCI4l8EP6tHu_IAiBo-8FmWiAGhgmSNylphro9R9oeU1L9paJpyEgwnyxSutcHViCDC6zwEiIE6rhEdGlaGix2GL36qfCQETJxbY_-2PjAQXBk0Tmb4re0YVjstev9AmbeUxw05fE69-jyAne5GjBlGrNeqYyIBhtmcS1zP3KjhFk56vdIHRjJuoZOV8f0PetqCPxzUAuK6Ktu7DmQQ-1KD5m3akshkM6DmY8oz8XIMx_3y_SU-7lfsCEEMhFTZOSkpKTd7xiXWiDXOJDy8edJsVHe7f0sco07vQttGyGA0eLNT5Xuh83sQCNUNpCo6bEtxqL0A701MUz_lLCSIjTfUJVo2IE-DSjPyOiujNdP52jValF9pcyf45POcUnE66izO1Pwxnh6o-Oir3Wj9RQCTtly6Q9VMI1gxGQZET0nz2xJSjloAuEwiBWSLF-cCaeFKTE5ds5mGhPxTyCz2w9Y9293Ws4qwwDDO_cTOgHYDUjjyO958qCsDV9tWdvbL7YPNzNF3DJUL3EFPlKlJ7J5xc0Z6cyPfOYiaTGPnZ0zWAxSvuflwRGviCd7uu8y4Bi_UzgDecY-umrlrHaFdhCbVC9HFDJayLfvFoIYhzXbPRV_PmLMIEmEyw4n5twQK7UNMs5RXZ2Ie-I_9GLH6lxElg7odYa3Hk4sAjzBmo6SjRnQAVaQW498VaKAlV1o8n3uRMduqDgNnvNiuASRrJrHPeMnLU";
 
             // load data
             HttpClient dataClient = new HttpClient();
-            var dataResponse = await tokenClient.GetAsync("https://calendar.fxstreet.com/v4.1/eventdate?authorization=" + token);
+            var dataResponse = await dataClient.GetAsync("https://calendar.fxstreet.com/v4.1/eventdate?authorization=" + token);
             if (!dataResponse.IsSuccessStatusCode)
             {
                 Console.WriteLine(dataResponse.StatusCode);
