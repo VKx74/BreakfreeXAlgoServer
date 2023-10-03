36cb2d45fb22aaed43af544b2aa4c439363343d9
Asger <asger@breakfreetrading.com>
2023-10-03 21:39:32 +0700

    disable auto trading.

--- src/AlgoserverAPI/Services/AutoTrading/AutoTradingParametersHelper.cs
+++ src/AlgoserverAPI/Services/AutoTrading/AutoTradingParametersHelper.cs
@@ -15,14 +15,14 @@ namespace Algoserver.API.Services
     {
         private static Dictionary<string, AutoTradingParameter> parameters = new Dictionary<string, AutoTradingParameter>
         {
-            {"USDCHF", new AutoTradingParameter {strength1h = 55, strength4h = 55, strength1d = 55, aroonPeriod = 55, aroonCount = 55}},
-            {"EURUSD", new AutoTradingParameter {strength1h = 33, strength4h = 66, strength1d = 66, aroonPeriod = 66, aroonCount = 66}},
-            {"EURCHF", new AutoTradingParameter {strength1h = 44, strength4h = 44, strength1d = 44, aroonPeriod = 44, aroonCount = 44}},
-            {"GBPUSD", new AutoTradingParameter {strength1h = 33, strength4h = 33, strength1d = 33, aroonPeriod = 33, aroonCount = 33}},
-            {"BTCUSD", new AutoTradingParameter {strength1h = 33, strength4h = 55, strength1d = 55, aroonPeriod = 55, aroonCount = 55}},
-            {"XAUUSD", new AutoTradingParameter {strength1h = 11, strength4h = 11, strength1d = 33, aroonPeriod = 33, aroonCount = 33}},
-            {"NZDCAD", new AutoTradingParameter {strength1h = 33, strength4h = 33, strength1d = 33, aroonPeriod = 33, aroonCount = 33}},
-            {"AUDNZD", new AutoTradingParameter {strength1h = 55, strength4h = 55, strength1d = 55, aroonPeriod = 55, aroonCount = 55}}
+            {"USDCHF", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 55, aroonCount = 55}},
+            {"EURUSD", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 66, aroonCount = 66}},
+            {"EURCHF", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 44, aroonCount = 44}},
+            {"GBPUSD", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 33, aroonCount = 33}},
+            {"BTCUSD", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 55, aroonCount = 55}},
+            {"XAUUSD", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 33, aroonCount = 33}},
+            {"NZDCAD", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 33, aroonCount = 33}},
+            {"AUDNZD", new AutoTradingParameter {strength1h = 999, strength4h = 999, strength1d = 999, aroonPeriod = 55, aroonCount = 55}}
         };
 
         public static int GetStrength1H(string symbol)
@@ -32,7 +32,7 @@ namespace Algoserver.API.Services
             {
                 return res.strength1h;
             }
-            return 44;
+            return 999;
         } 
         
         public static int GetStrength4H(string symbol)
@@ -42,7 +42,7 @@ namespace Algoserver.API.Services
             {
                 return res.strength4h;
             }
-            return 44;
+            return 999;
         }
 
         public static int GetStrength1D(string symbol)
@@ -52,7 +52,7 @@ namespace Algoserver.API.Services
             {
                 return res.strength1d;
             }
-            return 44;
+            return 999;
         }
         
         public static int GetAroonPeriod(string symbol)
