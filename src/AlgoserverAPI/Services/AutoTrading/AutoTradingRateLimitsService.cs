b70c5ca546225e8003b1bf0da676fbe88d3ec36f
andriy.yarosh <andriy.yarosh@gmail.com>
2023-10-11 18:35:49 +0300

    back ratelimits

--- src/AlgoserverAPI/Services/AutoTrading/AutoTradingRateLimitsService.cs
+++ src/AlgoserverAPI/Services/AutoTrading/AutoTradingRateLimitsService.cs
@@ -14,7 +14,6 @@ namespace Algoserver.API.Services
 
         public bool Validate(string id)
         {
-            return true;
             lock (_rates)
             {
                 if (_rates.ContainsKey(id))
