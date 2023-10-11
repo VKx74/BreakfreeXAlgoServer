115384d2e169ac5650ce9177e9cc8706314b0273
andriy.yarosh <andriy.yarosh@gmail.com>
2023-10-11 13:10:13 +0300

    changed default risks

--- src/AlgoserverAPI/Models/AutoTrading/UserInfoData.cs
+++ src/AlgoserverAPI/Models/AutoTrading/UserInfoData.cs
@@ -33,7 +33,7 @@ namespace Algoserver.API.Models
             useManualTrading = false;
             botShutDown = false;
             accountRisk = 30;
-            defaultMarketRisk = 5;
+            defaultMarketRisk = 12;
         }
 
         public void Validate()
@@ -60,7 +60,7 @@ namespace Algoserver.API.Models
 
             if (defaultMarketRisk <= 0)
             {
-                defaultMarketRisk = 5;
+                defaultMarketRisk = 12;
             }
         }
     }
