cce2bb71b60d428633183127bf39a6b79c265bb2
andriy.yarosh <andriy.yarosh@gmail.com>
2024-05-08 13:35:40 +0300

    logs

--- src/AlgoserverAPI/Services/Strategies/MonthDriveStrategy.cs
+++ src/AlgoserverAPI/Services/Strategies/MonthDriveStrategy.cs
@@ -1,3 +1,4 @@
+using System;
 using Algoserver.API.Helpers;
 using Algoserver.API.Models.REST;
 
@@ -5,25 +6,47 @@ namespace Algoserver.API.Services
 {
     public static class MonthDriveStrategy
     {
+        private static bool ShowLogs = true;
+
+        private static void WriteLog(string str)
+        {
+            if (!ShowLogs)
+            {
+                return;
+            }
+
+            Console.WriteLine($"MonthDriveStrategy >>> {str}");
+        }
+
         public static bool IsAutoTradeModeEnabled(AutoTradingSymbolInfoResponse symbolInfo, MESADataSummary mesaResponse)
         {
-            if (IsTooMatchVolatility(mesaResponse) || IsAutoTradeCapitulationConfirmed(symbolInfo))
+            if (IsTooMatchVolatility(mesaResponse))
             {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - volatility filter");
+                return false;
+            }
+            
+            if (IsAutoTradeCapitulationConfirmed(symbolInfo))
+            {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - capitulation filter");
                 return false;
             }
 
             if (!mesaResponse.TimeframePhase.TryGetValue(TimeframeHelper.MIN15_GRANULARITY, out var m15Phase))
             {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - 15min phase not exists");
                 return false;
             }
 
             if (!mesaResponse.TimeframePhase.TryGetValue(TimeframeHelper.MONTHLY_GRANULARITY, out var monthPhase))
             {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - month phase not exists");
                 return false;
             }
 
             if (m15Phase == PhaseState.CD)
             {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - 15min CD filter");
                 return false;
             }
 
@@ -31,48 +54,75 @@ namespace Algoserver.API.Services
 
             if (monthPhase == PhaseState.Drive && symbolInfo.MidGroupPhase == PhaseState.Drive && IsEnoughStrength(symbolInfo, minStrength))
             {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - active");
                 return true;
+            }
+            else
+            {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - month/mid group drive filter");
             }
 
             if (IsInGoodZoneOn4H(symbolInfo) && IsEnoughStrength(symbolInfo, minStrength))
             {
+                WriteLog($"{mesaResponse.Symbol} AutoMode - active is good on 4H zone");
                 return true;
             }
 
             return false;
-
         }
 
         public static bool IsHITLModeEnabled(AutoTradingSymbolInfoResponse symbolInfo, MESADataSummary mesaResponse)
         {
             if (IsAutoTradeCapitulationConfirmed(symbolInfo))
             {
+                WriteLog($"{mesaResponse.Symbol} HITL - capitulation filter");
                 return false;
             }
 
             if (symbolInfo.LongGroupPhase == PhaseState.Drive && symbolInfo.MidGroupPhase == PhaseState.Drive && IsEnoughStrength(symbolInfo, 10))
             {
+                WriteLog($"{mesaResponse.Symbol} HITL - active on stage 1");
                 return true;
+            }
+            else
+            {
+                WriteLog($"{mesaResponse.Symbol} HITL - 1 filter");
             }
 
             if (symbolInfo.ShortGroupPhase == PhaseState.Drive && symbolInfo.LongGroupPhase == PhaseState.Drive && symbolInfo.MidGroupPhase == PhaseState.Drive && IsEnoughStrength(symbolInfo, 8))
             {
+                WriteLog($"{mesaResponse.Symbol} HITL - active on stage 2");
                 return true;
+            }
+            else
+            {
+                WriteLog($"{mesaResponse.Symbol} HITL - 2 filter");
             }
 
             if (symbolInfo.CurrentPhase == PhaseState.Drive && IsEnoughStrength(symbolInfo, 10))
             {
+                WriteLog($"{mesaResponse.Symbol} HITL - active on stage 3");
                 return true;
+            }
+            else
+            {
+                WriteLog($"{mesaResponse.Symbol} HITL - 3 filter");
             }
 
             if (!mesaResponse.TimeframePhase.TryGetValue(TimeframeHelper.MONTHLY_GRANULARITY, out var monthPhase))
             {
+                WriteLog($"{mesaResponse.Symbol} HITL - month phase not exists");
                 return false;
             }
 
             if (monthPhase == PhaseState.Drive && symbolInfo.MidGroupPhase == PhaseState.Drive && IsEnoughStrength(symbolInfo, 10))
             {
+                WriteLog($"{mesaResponse.Symbol} HITL - active on stage 4");
                 return true;
+            }
+            else
+            {
+                WriteLog($"{mesaResponse.Symbol} HITL - 4 filter");
             }
 
             return false;
