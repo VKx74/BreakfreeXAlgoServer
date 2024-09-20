346b92bf5d79866d0b4fc3ed203d9322d6f788be
andriy.yarosh <andriy.yarosh@gmail.com>
2024-09-20 10:52:30 +0300

    xau model updated

--- src/AlgoserverAPI/Services/Strategies/SRLevelStrategies/StrategyForInstruments/v3/SRLevelStrategy_XAUUSD_v3.cs
+++ src/AlgoserverAPI/Services/Strategies/SRLevelStrategies/StrategyForInstruments/v3/SRLevelStrategy_XAUUSD_v3.cs
@@ -15,14 +15,24 @@ namespace Algoserver.Strategies.SRLevelStrategy.V3
             var settings = new SRLevelStrategySettings
             {
                 UseCatReflex = true,
-                CatReflexGranularity = TimeframeHelper.DAILY_GRANULARITY,
-                CatReflexPeriodReflex = 93,
-                CatReflexPeriodSuperSmoother = 17.6,
-                CatReflexPeriodPostSmooth = 141,
-                CatReflexConfirmationPeriod = 4,
-                CatReflexMinLevel = 0.02,
-                CatReflexMaxLevel = 1.8,
-                CatReflexValidateZeroCrossover = false
+                CatReflexGranularity = TimeframeHelper.HOUR4_GRANULARITY,
+                CatReflexPeriodReflex = 83,
+                CatReflexPeriodSuperSmoother = 104,
+                CatReflexPeriodPostSmooth = 116,
+                CatReflexConfirmationPeriod = 17,
+                CatReflexMinLevel = 0.09,
+                CatReflexMaxLevel = 2.3,
+                CatReflexValidateZeroCrossover = false,
+                
+                UseCatReflex2 = true,
+                CatReflexGranularity2 = TimeframeHelper.MIN1_GRANULARITY,
+                CatReflexPeriodReflex2 = 14,
+                CatReflexPeriodSuperSmoother2 = 27,
+                CatReflexPeriodPostSmooth2 = 441,
+                CatReflexConfirmationPeriod2 = 7,
+                CatReflexMinLevel2 = 0.05,
+                CatReflexMaxLevel2 = 3.4,
+                CatReflexValidateZeroCrossover2 = true
             };
 
             var result = await CalculateInternal(settings);
